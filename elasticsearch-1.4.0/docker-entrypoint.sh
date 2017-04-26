398a4cf42a5e4d3d9ff173c8094d41b02288e65a
Kyle Pollich <kpollich1@gmail.com>
2017-04-26 10:25:43 -0400

    Add elastic 1.4 image. Restructure

---
+++ elasticsearch-1.4.0/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# From https://github.com/docker-library/elasticsearch/blob/5727443ead111be19b55bfe5412dae5133cab8b1/1.7/alpine/docker-entrypoint.sh
+
+set -e
+
+# Add elasticsearch as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- elasticsearch "$@"
+fi
+
+# Drop root privileges if we are running elasticsearch
+# allow the container to be started with `--user`
+if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+	# Change the ownership of user-mutable directories to elasticsearch
+	for path in \
+		/usr/share/elasticsearch/data \
+		/usr/share/elasticsearch/logs \
+	; do
+		chown -R elasticsearch:elasticsearch "$path"
+	done
+	
+	set -- su-exec elasticsearch "$@"
+	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+# As argument is not related to elasticsearch,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$@"
